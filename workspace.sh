81492f464ae43b161698c4232fcc29d0f9d22b5c
Jamie Choi <jamie.choi.mail.2002@gmail.com>
2020-05-02 15:21:06 +0800

    Fix working directory with push and popd

--- workspace.sh
+++ workspace.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+pushd $(dirname $0)
 mkdir -p res
 touch template.cpp
 for i in a b c d e f; do
 	touch $i.cpp res/$i.in res/$i.out res/$i.ans res/$i.log;
 done
 vim -p a.cpp b.cpp c.cpp d.cpp e.cpp f.cpp template.cpp
+popd
