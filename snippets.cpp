731b40558c17fd493d9c347d46307ab40a5f0975
Jamie Choi <jamie.choi.mail.2002@gmail.com>
2020-06-27 02:25:20 +0800

    Add binary search snippets

--- snippets.cpp
+++ snippets.cpp
@@ -259,3 +259,31 @@ struct SegTreeLazy {
 	}
 };
 //! end segtreelazy
+
+//! snippet bs
+template<class T, class U>
+T lower_bound_on(T first, T last, const U& value, const function<U(T)>& fn) {
+	T it, step, count = last - first;
+	while (count > 0) {
+		it = first + (step = count / 2);
+		if (fn(it) < value)
+			first = ++it, count -= step + 1;
+		else
+			count = step;
+	}
+	return first;
+}
+
+template<class T, class U>
+T upper_bound_on(T first, T last, const U& value, const function<U(T)>& fn) {
+	T it, step, count = last - first;
+	while (count > 0) {
+		it = first + (step = count / 2);
+		if (!(value < fn(it)))
+			first = ++it, count -= step + 1;
+		else	
+			count = step;
+	}
+	return first;
+}
+//! end
