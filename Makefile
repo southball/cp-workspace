2e0d59453b88e7c9c5e230dcd78420d34eeefd98
Jamie Choi <jamie.choi.mail.2002@gmail.com>
2020-06-26 13:58:46 +0800

    Added template

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CPP = g++ -Wall -Wconversion -Wfatal-errors -g -std=c++14 -fsanitize=undefined,address
 
 %: %.cpp snippets.cpp
-	./preprocessor $@.cpp > /tmp/$@.proc.cpp
-	$(CPP) -O2 -o$@ /tmp/$@.proc.cpp
+	./preprocessor $@.cpp > ./$@.proc.cpp
+	$(CPP) -o$@ ./$@.proc.cpp
 
 preprocessor: preprocessor.cpp
 	$(CPP) -o$@ $<
--- snippets.cpp
+++ snippets.cpp
@@ -19,8 +19,8 @@ template<typename ...T> void die(T ...tok) { write(tok...); exit(0); }
 //! end io
 
 //! snippet opt
-#pragma gcc optimize("Ofast")
-#pragma gcc target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC target("avx2")
 //! end opt
 
 //! snippet modpow
@@ -101,3 +101,161 @@ struct NCR {
 	}
 };
 //! end ncr
+
+//! snippet monoid
+template<class T>
+struct Monoid {
+	T id;
+	virtual T op(T a, T b) = 0;
+};
+//! end monoid
+
+//! snippet segtree
+//! use monoid
+template<typename T, typename M, typename = enable_if<is_base_of<Monoid<T>, M>::value>>
+struct SegTree {
+	M monoid;
+	vector<T> tree;
+	int size;
+
+	SegTree(vector<T> elems, M monoid) : size(elems.size()), monoid(monoid) {
+		tree = vector<T>(2 * size);
+		for (int i = 0; i < size; i++)
+			tree[size + i] = elems[i];
+		build();
+	}
+
+	SegTree(int size, M monoid) : size(size), monoid(monoid) {
+		tree = vector<T>(size, monoid.id);
+		build();
+	}	
+
+	void build() {
+		for (int i = size - 1; i > 0; --i)
+			tree[i] = monoid.op(tree[i << 1], tree[i << 1 | 1]);
+	}
+
+	T query(int l, int r) {
+		T resl = monoid.id, resr = monoid.id;
+		++r; // handle inclusive query range
+		for (l += size, r += size; l < r; l >>= 1, r >>= 1) {
+			if (l & 1) resl = monoid.op(resl, tree[l++]);
+			if (r & 1) resr = monoid.op(tree[--r], resr);
+		}
+		return monoid.op(resl, resr);
+	}
+
+	T get(int p) {
+		return tree[size + p];
+	}
+
+	void update(int p, T val) {
+		for (tree[p += size] = val; p >>= 1;)
+			tree[p] = monoid.op(tree[p << 1], tree[p << 1 | 1]);
+	}
+};
+//! end segtree
+
+//! snippet monoidapply
+template<typename T, typename U>
+struct MonoidApply {
+	virtual T op(U fn, T val) = 0;
+};
+//! end monoidapply
+
+//! snippet segtreelazy
+//! use monoid
+//! use monoidapply
+template<typename T, typename MT, typename U, typename MU, typename F,
+	typename = enable_if<is_base_of<Monoid<T>, MT>::value>,
+	typename = enable_if<is_base_of<Monoid<U>, MU>::value>,
+	typename = enable_if<is_base_of<MonoidApply<T, U>, F>::value>>
+struct SegTreeLazy {
+	MU monoid_u;
+	MT monoid_t;
+	F apply;
+	int size;
+	vector<T> tree;
+	vector<U> lazy;
+
+	SegTreeLazy(int size, MT monoid_t, MU monoid_u, F apply) : size(size), monoid_t(monoid_t), monoid_u(monoid_u), apply(apply) {
+		tree = vector<T>(size * 4, monoid_t.id);
+		lazy = vector<U>(size * 4, monoid_u.id);
+	}
+
+	SegTreeLazy(vector<T> vals, MT monoid_t, MU monoid_u, F apply) : size(vals.size()), monoid_t(monoid_t), monoid_u(monoid_u), apply(apply) {
+		tree = vector<T>(size * 4, monoid_t.id);
+		lazy = vector<U>(size * 4, monoid_u.id);
+		_build(1, 0, vals.size() - 1, vals);
+	}
+
+	void _build(int id, int l, int r, const vector<T>& vals) {
+		if (l == r) {
+			tree[id] = vals[l];
+			return;
+		}
+	
+		int m = l + r >> 1;
+		_build(id << 1, l, m, vals);
+		_build(id << 1 | 1, m + 1, r, vals);
+		tree[id] = monoid_t.op(tree[id << 1], tree[id << 1 | 1]);
+	}
+	
+	void _pushdown(int id) {
+		lazy[id << 1] = monoid_u.op(lazy[id], lazy[id << 1]);
+		lazy[id << 1 | 1] = monoid_u.op(lazy[id], lazy[id << 1 | 1]);
+		lazy[id] = monoid_u.id;
+		_recalc(id);
+	}
+
+	T _query(int id, int l, int r, int tl, int tr) {
+		if (l == tl && r == tr) {
+			return apply.op(lazy[id], tree[id]);
+		}
+
+		int tm = tl + tr >> 1;
+		_pushdown(id);
+		if (r <= tm)
+			return _query(id << 1, l, r, tl, tm);
+		else if (l > tm)
+			return _query(id << 1 | 1, l, r, tm + 1, tr);
+		else
+			return monoid_t.op(
+					_query(id << 1, l, tm, tl, tm),
+					_query(id << 1 | 1, tm + 1, r, tm + 1, tr));
+	}
+
+	T query(int l, int r) {
+		return _query(1, l, r, 0, size - 1);
+	}
+
+	void _recalc(int id) {
+		tree[id] = monoid_t.op(
+				apply.op(lazy[id << 1], tree[id << 1]), 
+				apply.op(lazy[id << 1 | 1], tree[id << 1 | 1]));
+	}	
+
+	void _update(int id, int l, int r, int tl, int tr, U val) {
+		if (l == tl && r == tr) {
+			lazy[id] = monoid_u.op(val, lazy[id]);
+			return;
+		}
+		
+		_pushdown(id);
+		int tm = tl + tr >> 1;
+		if (r <= tm)
+			_update(id << 1, l, r, tl, tm, val);
+		else if (l > tm)
+			_update(id << 1 | 1, l, r, tm + 1, tr, val);
+		else
+			_update(id << 1, l, tm, tl, tm, val),
+			_update(id << 1 | 1, tm + 1, r, tm + 1, tr, val);
+
+		_recalc(id);
+	}
+
+	void update(int l, int r, U val) {
+		_update(1, l, r, 0, size - 1, val);
+	}
+};
+//! end segtreelazy
