68cf8b9341c7816c3bae414b6cbafefa47aca189
Jamie Choi <jamie.choi.mail.2002@gmail.com>
2020-07-19 20:55:50 +0800

    Add vector template

--- snippets.cpp
+++ snippets.cpp
@@ -51,10 +51,10 @@ struct Mod {
 		if (0 <= xx && xx < MOD) val = xx;
 		else val = (xx % MOD + MOD) % MOD;
 	}
-	Mod operator+=(Mod b) { if ((val += b.val) >= MOD) val -= MOD; return *this; }
-	Mod operator-=(Mod b) { if ((val += MOD - b.val) >= MOD) val -= MOD; return *this; }
-	Mod operator*=(Mod b) { (val *= b.val) %= MOD; return *this; }
-	Mod operator/=(Mod b) { (val *= b.inv().val) %= MOD; return *this; }
+	Mod& operator+=(Mod b) { if ((val += b.val) >= MOD) val -= MOD; return *this; }
+	Mod& operator-=(Mod b) { if ((val += MOD - b.val) >= MOD) val -= MOD; return *this; }
+	Mod& operator*=(Mod b) { (val *= b.val) %= MOD; return *this; }
+	Mod& operator/=(Mod b) { (val *= b.inv().val) %= MOD; return *this; }
 	Mod operator+(Mod b) const { Mod<MOD> tmp(*this); return tmp += b; }
 	Mod operator-(Mod b) const { Mod<MOD> tmp(*this); return tmp -= b; }
 	Mod operator*(Mod b) const { Mod<MOD> tmp(*this); return tmp *= b; }
@@ -126,7 +126,7 @@ struct SegTree {
 	}
 
 	SegTree(int size, M monoid) : size(size), monoid(monoid) {
-		tree = vector<T>(size, monoid.id);
+		tree = vector<T>(2 * size, monoid.id);
 		build();
 	}	
 
@@ -286,3 +286,42 @@ T upper_bound_on(T first, T last, const U& value, const function<U(T)>& fn) {
 	return first;
 }
 //! end bs
+
+//! snippet vector
+//! use root
+template<class T, int S>
+struct GVector {
+	T val[S];
+	
+	GVector() { rep(i, 0, S) val[i] = 0; }
+	GVector(array<T, S> init_val) { rep(i, 0, S) val[i] = init_val[i]; }
+
+	GVector& operator+=(GVector v) { rep(i, 0, S) val[i] += v.val[i]; return *this; }
+	GVector& operator-=(GVector v) { rep(i, 0, S) val[i] -= v.val[i]; return *this; }
+	GVector& operator*=(T v) { rep(i, 0, S) val[i] *= v; return *this; }
+	GVector& operator/=(T v) { rep(i, 0, S) val[i] /= v; return *this; }
+
+	GVector operator+(GVector v) { GVector<T, S> tmp(*this); return tmp += v; }
+	GVector operator-(GVector v) { GVector<T, S> tmp(*this); return tmp -= v; }
+	GVector operator*(T v) { GVector<T, S> tmp(*this); return tmp *= v; }
+	GVector operator/(T v) { GVector<T, S> tmp(*this); return tmp /= v; }
+
+	double norm2() { double r = 0; rep(i, 0, S) r += val[i] * val[i]; return r; }
+	double norm() { return sqrt(norm2()); }	
+	
+	GVector unit() { double scale = norm(); return scale == 0.0 ? *this : (*this / scale); }
+
+	friend istream& operator>>(istream &is, GVector &v) {
+		rep(i, 0, S) is >> v.val[i];
+		return is;
+	}
+	friend ostream& operator<<(ostream &os, const GVector& v) {
+		rep(i, 0, S - 1) os << v.val[i] << ' ';
+		os << v.val[S - 1];
+		return os;
+	}
+};
+
+using Vector2 = GVector<double, 2>;
+using Vector3 = GVector<double, 3>;
+//! end vector
