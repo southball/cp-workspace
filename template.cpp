899cfa5bde6e9b45e0d3471544b25bb35a24e692
Southball <jamie.choi.mail.2002@gmail.com>
2020-09-08 13:03:04 +0900

    Change template

--- template.cpp
+++ template.cpp
@@ -1,6 +1,8 @@
 #include "template/root.h"
 #include "template/io.h"
+#include "template/atcoder/all"
 using namespace std;
+using namespace atcoder;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
